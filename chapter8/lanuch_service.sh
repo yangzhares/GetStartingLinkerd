115d29ffecac4603f9ff538e1d0d53b6ac874a84
Clare Yang <yangzhangxian@gmail.com>
2018-01-21 20:58:03 +0800

    update launching script

--- chapter8/lanuch_service.sh
+++ chapter8/lanuch_service.sh
@@ -9,7 +9,7 @@ function rand(){
 
 PORT=$(rand 30000 65535)
 IP=$(ip addr show | grep eth1 | grep inet | awk '{print $2}' | cut -d'/' -f1)
-SERVICE_NAME=mesh
+SERVICE_NAME=service
 DOCKER_NAME=$SERVICE_NAME-$(cat /dev/urandom | head -n 10 | md5sum | head -c 10)
 
-docker run -d --network host --name $DOCKER_NAME -p $PORT:$PORT --env IP=$IP --env SERVICE_NAME=$SERVICE_NAME docker.io/zhanyang/helloworld:1.0 -addr :$PORT -text Mesh
+docker run -d --network host --name $DOCKER_NAME -p $PORT:$PORT --env http_proxy=localhost:4140 --env IP=$IP --env SERVICE_NAME=$SERVICE_NAME docker.io/zhanyang/helloworld:1.0 -addr :$PORT -text Service -target mesh.service.consul
